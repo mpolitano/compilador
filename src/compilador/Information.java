f61b7879c0af4b1e0980d4e03827d2a50a3a7819
NandoRaverta <nandoravert@gmail.com>
2014-09-03 11:16:50 -0300

    class information for symbol.value

---
+++ src/compilador/Information.java
@@ -0,0 +1,11 @@
+import java_cup.runtime.*;
+
+public class mySimbol extends Symbol{
+	
+	public String name;
+	public int intValue;
+	public float floatValue;
+	public int line;
+	public	int column;
+
+}
