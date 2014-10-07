1b4919d72fb59c07cbe8c15e546b6b6f22ac56ce
mpolitano <marianopolitano@hotmail.com>
2014-10-06 21:41:45 -0300

    change while (true)

--- src/compilador/execute.sh
+++ src/compilador/execute.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export CLASSPATH=.:/usr/share/java/JFlex.jar:/usr/share/java/cup.jar;
+export CLASSPATH=.:/usr/share/java/JFlex.jar:/usr/share/java/cup.jar
 jflex lexer.flex;
 cup analizadorSintactico.cup;
-javac -d . sym.java parser.java AnalizadorLexico.java
+javac ir/ast/*.java;
+javac ir/semcheck/*.java;
+javac ir/*.java;
+javac ir/error/*.java;
+javac ir/intermediateCode/*.java;
+javac -d . sym.java parser.java AnalizadorLexico.java;
