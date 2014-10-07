c97dfbcbeb62c30fb9ff20f336b9d613dd7efe0d
Nando Raverta <nando_ravert@hotmail.com>
2014-10-07 00:55:45 -0300

    upgrade execute script

--- src/compilador/execute.sh
+++ src/compilador/execute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CLASSPATH=.:/usr/share/java/JFlex.jar:/usr/share/java/cup.jar;
 jflex lexer.flex;
 cup analizadorSintactico.cup;
+export CLASSPATH=.:/usr/share/java/JFlex.jar:/usr/share/java/cup.jar;
 javac -d . sym.java parser.java AnalizadorLexico.java
